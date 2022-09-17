93315e9d5f7dfa17335549d8df9e1dacbd6f1e29
BogdanOleynik <bogdantiger2003@gmail.com>
2022-09-17 15:04:05 +0300

    first page beta

---
+++ triplanner-react/src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import './style.css'
+import FirstPageHomePage from './views/first-page-home-page'
+
+const App = () => {
+  return (
+    // <Router>
+    //   <div>
+    //     <Route
+    //       component={FirstPageHomePage}
+    //       exact
+    //       path="/first-page-home-page"
+    //     />
+    //   </div>
+    // </Router>
+    <FirstPageHomePage></FirstPageHomePage>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
